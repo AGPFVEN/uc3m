73468766cb3b6e95d61ab3a30d5cb1157208b74c
Alfonso <alfonso_pineda2003@hotmail.com>
2021-09-07 23:53:51 +0200

    Exercise 10

---
+++ 1st_year/1st_term/programming/3rd_week/Exercise_10.py
@@ -0,0 +1,9 @@
+#This program shows how to store strigs in the most strange way
+
+twinkle_twinkle = """   how I wonder what you are!
+        Up above the world so high,
+        like a diamond in the sky.
+Twinkle, twinkle, little star,
+    how I wonder what you are."""
+
+print(twinkle_twinkle)
