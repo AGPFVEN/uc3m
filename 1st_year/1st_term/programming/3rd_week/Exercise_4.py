03790c55e4500112b44e723451bdaf2c085f078b
Alfonso <alfonso_pineda2003@hotmail.com>
2021-09-11 21:47:38 +0200

    Exercise 4 (finished)

--- 1st_year/1st_term/programming/3rd_week/Exercise_4.py
+++ 1st_year/1st_term/programming/3rd_week/Exercise_4.py
@@ -1,2 +1,16 @@
-#This program will change the a variable from int to str
+#This program will change the value of a variable from int to string
 
+#Initializing the variable
+change_var = 2
+
+#Print the variable at the beginning and the type
+print("change_var is %i of type" % change_var, str(type(change_var)))
+
+#Change the value of variable
+change_var = "Hello"
+
+#Print the variable at the end and the type
+print("change_var is %s of type" % change_var, str(type(change_var)))
+
+#It doesn't matter the type of the varieble when you change it's value
+#This propperty is named Dynamic typing
