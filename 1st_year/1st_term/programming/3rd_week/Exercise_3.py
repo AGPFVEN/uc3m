49c571b853d6aa36bd80147500f1bcf960427781
Alfonso <alfonso_pineda2003@hotmail.com>
2021-09-21 21:31:24 +0200

    Exercise 3

---
+++ 1st_year/1st_term/programming/3rd_week/Exercise_3.py
@@ -0,0 +1,10 @@
+#This program will change the value of a variable
+
+#Initializing the variable
+change_var = 2
+
+#Print the variable at the beginning and the type
+print("change_var is %i of type" % change_var, str(type(change_var)))
+
+#Change the value of variable
+change_var = 2.2
