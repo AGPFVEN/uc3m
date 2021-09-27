1d0b39335d39fe8c17d926927a40e314a69233b1
Alfonso <alfonso_pineda2003@hotmail.com>
2021-09-27 18:32:15 +0200

    Exercise 12 (finished)

--- 1st_year/1st_term/programming/3rd_week/Exercise_12.py
+++ 1st_year/1st_term/programming/3rd_week/Exercise_12.py
@@ -1,9 +1,14 @@
+#This program shows another way to concatenate strings
+
+#Declaring Strings
 name = 'Johnny Depp'
 age = 55
 height = 1.78
 weight = 65.4
 eyes = 'brown'
 hair = 'brown'
+
+#Printing them
 print("Let’s talk about ", name, ".")
 print("He's", age, " years old")
 print("He’s ", height, " meters tall.")
