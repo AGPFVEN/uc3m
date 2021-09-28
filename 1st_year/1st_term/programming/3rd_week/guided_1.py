53a45ac1772696a4750e1e606ebf9cff387d8448
Alfonso <alfonso_pineda2003@hotmail.com>
2021-09-28 18:14:48 +0200

    Guided exercise

--- 1st_year/1st_term/programming/3rd_week/guided_1.py
+++ 1st_year/1st_term/programming/3rd_week/guided_1.py
@@ -1,26 +1,34 @@
-"""This is the first guided exercie:
-It is needed to fill the gaps in the """
+"""This is the first guided exercie"""
 
-#This text is the given one (the %s are the gaps which must be filled with the answers of the user)
-text = """Given the current situation that King’s Landing is facing, the House %s is asking for your economic services because
-of the following cause %s.
-The estimated total quantity is %s gold coins. The loan will be returned during the %s following years with
-%s of bank interest. Thus, the money recovered from the bank, once the loan is completely returned, will be %s
-gold coins
-I hope the bank will consider this proposal because the House %s always pays its debts."""
 
-first_input = input("Name of the house ")
 
-second_input = input("Name of a service")
+first_part = """Given the current situation that King’s Landing is facing, the House """
+second_part = """is asking for your economic services because
+of the following cause."""
+third_part = """
+The estimated total quantity is"""
+fourth_part = """gold coins. The loan will be returned during the """
+fifth_part = """> following years with
+"""
+seventh_part = """I hope the bank will consider this proposal because the House"""
 
-third_input = float(input("Quantity of gold you want to get"))
 
-fourth_input = input("Numbers of years you need to return the previous quantity")
 
-fifth_input = float(input("Interest of the loan" + "%"))
+print("In this program I will help you to fill the form to ask money")
+first_input = input("Please enter the name of the house ")
 
-sixth_input = third_input + (third_input * fifth_input)
 
-seventh_input = first_input
+second_input = input("")
 
-print(text % (first_input, second_input, third_input, fourth_input, fifth_input, sixth_input, seventh_input))
+third_input = input("")
+
+fourth_input = input("")
+
+
+fifth_input = input("")
+
+sixth_input = input("")
+
+seventh_input = input("")
+
+eighth_input = input("")
