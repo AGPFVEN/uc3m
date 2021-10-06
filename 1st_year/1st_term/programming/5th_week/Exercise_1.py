78843e327400dcd41eef276fd031db108bc28a4f
Alfonso <alfonso_pineda2003@hotmail.com>
2021-10-06 16:58:54 +0200

    Exercise 1 (with comments)

--- 1st_year/1st_term/programming/5th_week/Exercise_1.py
+++ 1st_year/1st_term/programming/5th_week/Exercise_1.py
@@ -1,8 +1,10 @@
 import random
 
+#Generate a random number and ask for another one
 number = random.randrange(1, 21)
 n = int(input("Guess the secret number:\n"))
 
+#Loop till the user guess the secret number
 while (n != number):
     n = int(input("Bad luck... Try again:\n"))
 
