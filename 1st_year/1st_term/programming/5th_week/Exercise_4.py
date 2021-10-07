e3e498d3a020090f2b9884105167fb371ffc0859
Alfonso <alfonso_pineda2003@hotmail.com>
2021-10-07 15:23:04 +0200

    Exercise 4

---
+++ 1st_year/1st_term/programming/5th_week/Exercise_4.py
@@ -0,0 +1,19 @@
+#Ask for a "number" and declaring a controller variable to check if the input is not a number
+inp = input("Enter a number: ")
+controller = True
+
+#Loop to check is the input is not a number
+for number in inp:
+    if(number.isdigit() == False):
+        controller = False
+
+#loop to try again
+while(controller != True):
+    inp = input("You entered a wrong input, input a real number: ")
+    controller = True
+    for number in inp:
+        if(number.isdigit() == False and number != "."):
+            controller = False
+
+#Result
+print("The square of ", inp, " is ", float(inp) ** 2)
