a468bd219752b010797966497a68c2697b91d8c3
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-03-02 12:33:45 +0100

    Update problem4.vhdl

--- 1st_year/2nd_term/Computer Technology/3rd-5th week/problem4.vhdl
+++ 1st_year/2nd_term/Computer Technology/3rd-5th week/problem4.vhdl
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity problem4 is
     port(
-        A, B, Q: in std_logic;
-        K in bit_vector;
+        A, B: in std_logic;
+        K, Q in std_logic_vector(2 downto 0);
         X out std_logic;
-        P out bit_vector
+        P out std_logic_vector(2 downto 0)
     );
 end problem4;
