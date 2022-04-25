5888f472536d06838305ebfa3d700765f75be204
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-04-25 02:50:54 +0200

    updating all uc3m code

--- 1st_year/2nd_term/Data Structures and Algorithms/phase2/phase2_ongoing/phase2.py
+++ 1st_year/2nd_term/Data Structures and Algorithms/phase2/phase2_ongoing/phase2.py
@@ -46,6 +46,7 @@ class AVLTree(BinarySearchTree):
             if difference_heights > balance_factor:
                 #Simple
                 if self._height(right_original.right) > self._height(right_original.left):
+                    print("simple left rot")
                     auxiliar_tree = node.right.left
                     tree_balanced._root = right_original
                     tree_balanced._root.left = node
@@ -53,6 +54,7 @@ class AVLTree(BinarySearchTree):
                 
                 #Double
                 else:
+                    print("double left rot")
                     tree_balanced._root = right_original.left
                     auxiliar_tree = right_original.left.left
                     tree_balanced._root.left = node
@@ -64,6 +66,7 @@ class AVLTree(BinarySearchTree):
             else:
                 #Simple
                 if self._height(left_original.left) > self._height(left_original.right):
+                    print("simple right rot")
                     auxiliar_tree = node.left.right
                     tree_balanced._root = left_original
                     tree_balanced._root.right = node
@@ -71,6 +74,7 @@ class AVLTree(BinarySearchTree):
 
                 #Double
                 else:
+                    print("double right rot")
                     tree_balanced._root = left_original.right
                     tree_balanced._root.left = left_original
                     tree_balanced._root.right = node
@@ -85,18 +89,35 @@ class AVLTree(BinarySearchTree):
 
             return node
 
-"""  remove(8), 7 is unbalanced, right rotation with subtrees """
 avl = AVLTree()
-data = [17, 8, 12, 2, 7, 27, 48, 58, 3, 4]
+##Test21 (It's not the same as the given by teachers but it is the same as the web page AVL tree simulator)
+#"""  remove(8), 7 is unbalanced, right rotation with subtrees """
+#data = [17, 8, 12, 2, 7, 27, 48, 58, 3, 4]
+#for n in data:
+    #avl.insert(n)
+    #avl.draw()
+
+## we remove 8 (root), it will be replaced with 7
+#avl.remove(8)  # root
+#avl.draw()
+
+#data = [12, 3, 27, 2, 7, 17, 48, 4, 58]
+#expected = BinarySearchTree()
+#for n in data:
+    #expected.insert(n)
+#expected.draw()
+
+#Test 22
+"""  remove(17) -> 27 is unbalance; left rotation with subtrees with subtrees """
+data = [17, 8, 12, 2, 7, 27, 48, 45, 58, 3, 4]
 for n in data:
     avl.insert(n)
-     avl.draw()
+    avl.draw()
 
-# we remove 8 (root), it will be replaced with 7
-avl.remove(8)  # root
+avl.remove(17)  # root
 avl.draw()
 
-data = [12, 3, 27, 2, 7, 17, 48, 4, 58]
+data = [12, 7, 48, 3, 8, 27, 58, 2, 4, 45]
 expected = BinarySearchTree()
 for n in data:
     expected.insert(n)
