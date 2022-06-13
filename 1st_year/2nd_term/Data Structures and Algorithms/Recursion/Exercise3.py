7d6561bef5e549a6dc8949af32d4f34003c24630
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-06-13 13:24:12 +0200

    Testing linux environment

--- 1st_year/2nd_term/Data Structures and Algorithms/Recursion/Exercise3.py
+++
@@ -1,7 +0,0 @@
-def sumdigits(n):
-    if n < 10:
-        return n
-    else:
-        return n % 10 + sumdigits(int(n / 10))
-
-print(sumdigits(2356))
--- 1st_year/2nd_term/Data Structures and Algorithms/Recursion/recursion_example.py
+++
@@ -1,9 +0,0 @@
-def rec(a):
-    if a > 3:
-        return a
-
-    print("hello " + str(a))
-    rec(a + 1)
-    print("bye" + str(a))
-
-print(rec(1))
