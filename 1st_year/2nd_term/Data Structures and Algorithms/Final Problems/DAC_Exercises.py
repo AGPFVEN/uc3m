a99a7c2582b87d532d51d8a2573f12344a9a2b30
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-06-19 01:49:50 +0200

    Divide and Conquer Exercises

--- 1st_year/2nd_term/Data Structures and Algorithms/Final Problems/DAC_Exercises.py
+++ 1st_year/2nd_term/Data Structures and Algorithms/Final Problems/DAC_Exercises.py
@@ -23,4 +23,77 @@ def smallest_even_number(data:list):
         else:
             return right
 
-print(smallest_even_number([1, 3, 4, 5, 7, 9, 2, -2, 5, 3, 10, 8]))
+def getIndexes(data,x):
+    if data==None or len(data)==0:
+        return []
+    return _getIndexes(data,x,0,len(data)-1)
+
+def _getIndexes(data,x,left,right):
+    print(left, right)
+
+    if left>right:
+        return []
+    
+    if left==right:
+      
+       if data [left] == x:
+           return [left]
+       else:
+           return []
+       
+    mid = (left + right) // 2
+
+    index1= _getIndexes(data,x,left,mid)
+      
+    index2= _getIndexes(data,x,mid+1,right)
+   
+    return index1 + index2
+
+
+def kk(ls:list):
+    return kk_(ls, 0, len(ls) - 1)
+def kk_(ls:list, first, second):
+    res = []
+    mid = (second + first) // 2 
+    print(first, mid, second, res)
+
+    if first == second:
+        return [first]
+ 
+    if first < second:
+        #left
+        for i in (kk_(ls, first, mid)):
+            res.append(i)
+
+        #right
+        for j in (kk_(ls, mid + 1, second)):
+            res.append(j)
+
+    return res
+
+
+def multiples_sum(ls:list, x:int):
+    if len(ls) < 1:
+        return 0
+
+    elif len(ls) == 1:
+        if ls[0] % x == 0:
+            return ls[0]
+
+        return 0
+
+    else:
+        mid = ((len(ls)) // 2)
+
+        left = multiples_sum(ls[:mid], x)
+        right = multiples_sum(ls[mid:], x)
+
+        return left + right
+            
+
+l = [1,2,3,4,2,3,4,7,8,9,2,2,4,2]
+#print(l[(len(l) - 1)// 2])
+res = multiples_sum(l, 2)
+#print(len(res))
+print(res)
+#print(len(l))
