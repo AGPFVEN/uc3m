476874a8a99d2919d574b8fe3d6dc71374a76b84
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-09-28 17:29:03 +0200

    Possible solution to RISC-V (assambly) laboratory

--- 2nd_year/computer structures/1st lab/1st_part.s
+++ 2nd_year/computer structures/1st lab/1st_part.s
@@ -1,38 +1,43 @@
 .data
 	A: .string "Te1"
-    B: .string "Te1"
+    B: .string "Te"
 
 .text
-	main:
-		#string_compare(A, B)    
-		la a0 A					#Argument A
-    	la a1 B 				#Argument B
-        jal ra string_compare 	#Invoke
-        
-        #Print result
-        li  a7, 1
-        ecall
-        
+main:
+    #string_compare(A, B)    
+    la a0 A					#Argument A
+    la a1 B 				#Argument B
+    jal ra string_compare 	#Invoke        
 
-	string_compare:
-    	#Load strings in Temporary Int Registers
-    	lb t0, 0(a0)
-    	lb t1, 0(a1)
+string_compare:
+    #------------Question-------------------
+    #Do the solution of the problem of the exercise (-1 result) has to be inside
+    #of the subroutine or can it be tested before entering the subroutine
+
+    #Load strings in Temporary Int Registers
+    lb t0, 0(a0)
+    lb t1, 0(a1)
         
-        #Pass to the next char
-        addi a0, a0, 1
-        addi a1, a1, 1
+    #Pass to the next char
+    addi a0, a0, 1
+    addi a1, a1, 1
         
-      	#Check if the current chars are different
-        bne t0 t1 not_equal
+    #Check if the current chars are different
+    bne t0, t1, not_equal
         
-        #If the current chars are equal and are the zero (the end)
-        bne t0 zero string_compare
-        la a0 1						#If it's equal to 0 result = 1
-        j end
+    #If the current chars are equal and are the zero (the end)
+    bne t0, zero, string_compare
+    li a0, 1						#If it's equal to 0 result = 1
+    j end
     
-    not_equal:
-        la a0 0
 
-    end:
-        jr ra
+not_equal:
+    li a0, 0						#If it's not equal result = 0
+    
+end:
+    #Why can't I use
+    #jr ra -------------here-------------
+
+    #Print result
+    li  a7, 1
+    ecall
