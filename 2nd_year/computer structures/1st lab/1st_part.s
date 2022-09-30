b710dbefc6cd7a3f8a959d6f4f72800b5d6d23af
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-09-30 19:20:50 +0200

    Full 1st exercise of 1st lab

--- 2nd_year/computer structures/1st lab/1st_part.s
+++ 2nd_year/computer structures/1st lab/1st_part.s
@@ -1,19 +1,21 @@
 .data
 	A: .string "Te1"
-    B: .string "Te"
+    B: .string "Te1"
 
 .text
 main:
     #string_compare(A, B)    
     la a0 A					#Argument A
     la a1 B 				#Argument B
-    jal ra string_compare 	#Invoke        
+    jal ra string_compare 	#Invoke
+    li a7, 10				#End Program (with syscall 10)
+    ecall
 
 string_compare:
-    #------------Question------------------- dentro de string compare
-    #Do the solution of the problem of the exercise (-1 result) has to be inside
-    #of the subroutine or can it be tested before entering the subroutine
-
+    beq a0, zero, error				#If a string is empty go to error
+    beq a1, zero, error
+    
+loop:
     #Load strings in Temporary Int Registers
     lb t0, 0(a0)
     lb t1, 0(a1)
@@ -26,18 +28,16 @@ string_compare:
     bne t0, t1, not_equal
         
     #If the current chars are equal and are the zero (the end)
-    bne t0, zero, string_compare
+    bne t0, zero, loop
     li a0, 1						#If it's equal to 0 result = 1
     j end
-    
+
+error:								#If a string is empty, return -1
+	li a0, -1
+    j end
 
 not_equal:
     li a0, 0						#If it's not equal result = 0
     
 end:
-    #Why can't I use need t break the loop using syscall 10 (exit)
-    jr ra #-------------here-------------
-
-    #Print result
-    #li  a7, 1
-    #ecall
+    jr ra							#Return to Main (exit fuction/subroutine)
