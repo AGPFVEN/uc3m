4af31826fbac082ae85ce3deeaf3a1b68a93aeb6
Alfonso Pineda <agpfven@gmail.com>
2023-09-27 17:56:03 +0200

    Small progress

--- 2nd_year/Computer Structures/1st lab (second year)/lab.s
+++ 2nd_year/Computer Structures/1st lab (second year)/lab.s
@@ -6,9 +6,13 @@
     li a7, 10           #End program
     ecall
 
-    factorial:
-    li t0, 0            #Counter
-    li t1, 1            #Result
+    e: #Until 6
+    li t0, 0            #E counter
+    li t1, 0            #E result
+    
+    factorial:          #Recives (a0) Uses (t01) Return (a0)
+    li t0, 0            #Factorial Counter
+    li t1, 1            #Factorial Result
 
     factorial_loop:
     beq t0 a0 end_factorial
