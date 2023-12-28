169bf59ecf50ea7ec7d4b80dddfc557d4e44d276
Alfonso Pineda <agpfven@gmail.com>
2023-12-28 13:22:06 +0100

    Assembly exercises

---
+++ 2nd_year/Computer Structures/Assembly Exercises/matrix_multiplication.s
@@ -0,0 +1,43 @@
+.data
+    m1: .word 3, 3
+        .word 8, 4, 5
+        .word 0, 9, 7
+        .word 4, 4, 1
+
+    m2: .word 3, 3
+        .word 6, 3, 2
+        .word 1, 5, 8
+        .word 7, 9, 4
+
+.text
+    main:
+    #Read Inputs
+    la a0, m1
+    la a1, m2
+
+    #Call Matrix_mul
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    j Matrix_mul
+
+    Matrix_mul:
+    #Copy matrix addresses
+    mv s0, a0   #s0 = m1
+    mv s1, a1   #s0 = m2
+
+    #Check matrix dimensions
+    sw s2, 0(s0)    #s2 -> dimensions of m1
+    sw s3, 4(s1)    #s3 -> dimensions of m2
+    bne s2, s3, end_mul
+    sw s2, 4(s0)
+    sw s3, 0(s1)
+    bne s2, s3, end_mul
+
+    #Set up matrix counters
+    li s2, 0    #s2 -> matrix counter
+
+    #End multiplication function
+    end_mul:
+    lw ra 0(sp)
+    addi sp, sp, 4
+    jal ra
