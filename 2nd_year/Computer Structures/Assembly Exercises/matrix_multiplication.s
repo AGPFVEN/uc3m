0287f7518347889122bb925e544c4568a7383b67
Alfonso Pineda <agpfven@gmail.com>
2024-01-16 23:28:46 +0100

    Progress in some projects

--- 2nd_year/Computer Structures/Assembly Exercises/matrix_multiplication.s
+++ 2nd_year/Computer Structures/Assembly Exercises/matrix_multiplication.s
@@ -1,13 +1,12 @@
 .data
-    m1: .word 3, 3
+    m1: .word 2, 3
         .word 8, 4, 5
         .word 0, 9, 7
-        .word 4, 4, 1
 
-    m2: .word 3, 3
-        .word 6, 3, 2
-        .word 1, 5, 8
-        .word 7, 9, 4
+    m2: .word 3, 2
+        .word 6, 3
+        .word 1, 5
+        .word 7, 9
 
 .text
     main:
@@ -27,17 +26,32 @@
 
     #Check matrix dimensions (Check if multiplication is possible)
     sw s2, 0(s0)    #s2 -> dimensions of m1 (Rows)
-    sw s3, 4(s1)    #s3 -> dimensions of m2 (Columns)
+    sw s3, 4(s1)    #s3 -> dimensions of m2 (Columns)----
     bne s2, s3, end_mul
-    sw s2, 4(s0)
-    sw s3, 0(s1)
-    bne s2, s3, end_mul
+    sw s4, 4(s0)    #s4 -> dimensions of m1 (Columns)
+    sw s5, 0(s1)    #s5 -> dimensions of m2 (Rows)
 
-    #Set up matrix counters
-    sw s3, 4(s0)    #s3 -> Dimensions of m1 (Columns)
-    mul s2, s2, s3  #s2 -> Counter (Limit)
-    li s3, 0        #s3 -> Counter (Actual position)
-    li s4, 0        #s4 -> Counter of m2 (Actual position)
+    #Auxiliar data
+    mul s2, s2, s3  #s2 -> number of elements in result
+
+
+
+
+
+    mul s6, s2, s4  #s6 -> counter of m1 (numbers in matrix)
+    addi s7, s5, -1
+    mul s8, s8, s7
+    addi s8, s8, -1 #s8 -> Column switcher
+    mul s7, s3, s5  #s7 -> counter of m2 (numbers in matrix)
+
+    #Move auxiliar to optimize space (s3 is already placed)
+    mv s2, s6       #s2 -> counter of m1 (numbers in matrix)
+    mv s4, s7       #s7 -> counter of m2 (numbers in matrix)
+    mv s5, s8       #s8 -> Column switcher of m2
+
+    #First loop 
+
+
 
     #End multiplication function
     end_mul:
