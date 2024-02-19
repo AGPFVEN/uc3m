f028f15d50cc4065d97bbd3080ad4a3892edc5f9
Alfonso Pineda <agpfven@gmail.com>
2024-02-19 23:25:49 +0100

    Almost first function

--- 2nd_year/Operating Systems/2nd time/Lab 1/mywc.c
+++ 2nd_year/Operating Systems/2nd time/Lab 1/mywc.c
@@ -8,24 +8,28 @@
 int main(int argc, char *argv[])
 {
 	/*If less than two arguments (argv[0] -> program, argv[1] -> file to process) print an error y return -1*/
-	if(argc < 2)
-	{
+	if(argc < 2){
 		printf("Too few arguments\n");
 		return -1;
 	}
 
-	//Open file
+	// open file
 	int fd = open(argv[1], O_RDONLY);
 	if (fd == -1){
 		printf("Error at opening file\n");
 		return -1;
 	}
 
-	//Read file
-	char* file_buffer = malloc(sizeof(char));
-	read(fd, file_buffer, sizeof(char));
+	// declare variables in order to read file
+	char* buffer_file = malloc(sizeof(char));
+	int buffer_size = 1;
+	ssize_t data;
 
-	printf("%s\n", file_buffer);
+	// read
+	do{
+		data = read(fd, buffer_file, buffer_size);
+		printf("%s\n", buffer_file);
+	} while (data == buffer_size * sizeof(char));
 
 	return 0;
 }
