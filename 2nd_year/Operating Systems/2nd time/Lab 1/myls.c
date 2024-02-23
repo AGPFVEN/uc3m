e5d6590019575da6fb6420ce20fa748c497f6a61
Alfonso Pineda <agpfven@gmail.com>
2024-02-23 18:22:59 +0100

    Another function ready, too easy

--- 2nd_year/Operating Systems/2nd time/Lab 1/myls.c
+++ 2nd_year/Operating Systems/2nd time/Lab 1/myls.c
@@ -9,7 +9,36 @@
 
 int main(int argc, char *argv[])
 {
-	
+	// string containing path 
+	char *givenPath;
+
+	// select path
+	if (argc < 2){
+		givenPath = "./";
+	} else {
+		givenPath = argv[1];
+	}
+
+	// pointer to DIR object
+	DIR *dir;
+
+	// open directory
+	dir = opendir(givenPath);
+	if (dir == NULL){
+		printf("Error at openning directory '%s'\n", givenPath);
+		return -1;
+	}
+
+	// dirent structure indicating next directory entry
+	struct dirent  *entry;
+
+	// read contents of directory
+	while ((entry = readdir(dir)) != NULL){
+		printf("%s\n", entry->d_name);
+	}
+
+	// close directory
+	closedir(dir);
 
 	return 0;
 }
