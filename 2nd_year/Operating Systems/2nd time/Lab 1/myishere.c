c76981805b4d64a761b2863b1b4ac7e25184acd1
Alfonso Pineda <agpfven@gmail.com>
2024-02-27 17:00:35 +0100

    Finished all functions

--- 2nd_year/Operating Systems/2nd time/Lab 1/myishere.c
+++ 2nd_year/Operating Systems/2nd time/Lab 1/myishere.c
@@ -15,5 +15,41 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	// pointer to DIR object
+	DIR *dir;
+
+	// open directory
+	dir = opendir(argv[1]);
+	if (dir == NULL){
+		printf("Error at openning directory \n");
+		return -1;
+	}
+
+	// dirent structure indicating next directory entry
+	struct dirent  *entry;
+
+	// integer that works as a bool to check if the condition is complete
+	int checker = 0;
+
+	// read contents of directory
+	while (((entry = readdir(dir)) != NULL) && checker != 1){
+		
+		// check if entry name (name of file) is equal to argument
+		if (strcmp(entry->d_name, argv[2]) == 0){
+			checker++;
+		}
+
+	}
+
+	// print result
+	if (checker == 0){
+		printf("File %s is not in directory %s\n", argv[2], argv[1]);
+	} else {
+		printf("File %s is in directory %s\n", argv[2], argv[1]);
+	}
+
+	// close directory
+	closedir(dir);
+
 	return 0;
 }
